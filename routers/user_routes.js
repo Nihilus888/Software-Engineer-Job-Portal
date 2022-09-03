ca80911cd2120433ab5f6909d85ac4569831d7ff
Elliott Tay <austonmartin@gmail.com>
2022-09-03 13:45:02 +0800

    add basic routes framework and changed controller to user_controller.js

---
+++ routers/user_routes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const router = express.Router()
+
+//register route
+
+//login route
+
+//logout route
+
+//profile route with creating jobs and jobs that I applied to 
