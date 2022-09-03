a18de52e7b01562d167d441743b66c9254d5da92
Elliott Tay <austonmartin@gmail.com>
2022-09-03 17:25:46 +0800

    working on authentication middleware

--- seed/seedUser.js
+++ seed/seedUser.js
@@ -1,7 +1,7 @@
 //require dotenv file to encode password
 require('dotenv').config()
 const mongoose = require("mongoose");
-const User = require("./models/user");
+const User = require("../models/user");
 
 const connStr = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@generalassembly.odxzs.mongodb.net`;
 
