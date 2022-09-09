033cafd45d9d9d3e394397ef4851685e39464fb9
Elliott Tay <austonmartin@gmail.com>
2022-09-09 19:01:07 +0800

    still working on fixing skills array issue

--- controllers/validators/users.js
+++ controllers/validators/users.js
@@ -1,22 +1,21 @@
-const Joi = require('joi')
-const { joiPasswordExtendCore } = require('joi-password');
+const Joi = require("joi");
+const { joiPasswordExtendCore } = require("joi-password");
 const JoiPassword = Joi.extend(joiPasswordExtendCore);
 
 const userValidators = {
-    createUser: Joi.object({
-        name: Joi.string().min(5).required(),
-        email: Joi.string().min(9).required(),
-        password: JoiPassword.string().min(5).noWhiteSpaces().required(),
-        //confirmPassword: Joi.string().required().valid(Joi.ref('password')),
-        job: Joi.string().min(3).required(),
-        position: Joi.string().min(3),
-        experience: Joi.number().min(1).required(),
-        //skills: Joi.array().items(Joi.string()),
+  createUser: Joi.object({
+    name: Joi.string().min(5).required(),
+    email: Joi.string().min(9).required(),
+    password: JoiPassword.string().min(5).noWhiteSpaces().required(),
+    //confirmPassword: Joi.string().required().valid(Joi.ref('password')),
+    job: Joi.string().min(3).required(),
+    position: Joi.string().min(3),
+    experience: Joi.number().min(1).required(),
+    skills: Joi.array().items(Joi.object().keys().min(1)),
+    if(err) {
+      console.log(err);
+    },
+  }),
+};
 
-        if(err) {
-            console.log(err)
-        }
-    })
-}
-
-module.exports = userValidators
+module.exports = userValidators;
