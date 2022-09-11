a4a06bcada952543429846a66cc12fbc1c509955
Lutfi Aman <lunfy@Lutfi-MBP.local>
2022-09-11 23:44:23 +0800

    user routes completed, pending integration

--- routers/user_routes.js
+++ routers/user_routes.js
@@ -1,13 +1,20 @@
 const express = require('express')
+const userController = require('../controllers/user_controller')
+const authMiddleware = require('../middleware/auth_middleware')
+const user = require('../models/user')
 
 const router = express.Router()
 
 //register route
+router.post('/register', userController.register)
 
 //login route
+router.post('/login', userController.login)
 
 //logout route
+router.post('/logout', userController.logout)
 
 //profile route with creating jobs and jobs that I applied to 
+router.get('/profile', authMiddleware, userController.profile)
 
 module.exports = router
