7a8e395f5717ffb574216d7494df800094fd7988
Elliott Tay <austonmartin@gmail.com>
2023-01-09 16:17:53 +0800

    fixing git error

--- controllers/jobs_controller.js
+++ controllers/jobs_controller.js
@@ -1,52 +1,52 @@
-const jwt_decode = require('jwt-decode');
+const jwt_decode = require("jwt-decode");
 const jwt = require("jsonwebtoken");
 const postJobModel = require("../models/posting_jobs");
 const savedJobModel = require("../models/saved_jobs");
 const jobVal = require("./validators/jobs");
 const editJobVal = require("./validators/edit_jobs");
-const { default: jwtDecode } = require('jwt-decode');
-const mongoose = require('mongoose')
-
+const { default: jwtDecode } = require("jwt-decode");
+const mongoose = require("mongoose");
 
 module.exports = {
-  listJobs: async (req,res) => {
+  listJobs: async (req, res) => {
     // perform API call to respective API from external party
     // list all jobs in JSON format
-    const searchStr = req.body.search
-    let pg = req.body.pg
+    const searchStr = req.body.search;
+    let pg = req.body.pg;
 
-    if(!pg) {
-        pg = 1
+    if (!pg) {
+      pg = 1;
     }
-    const response = await fetch(`https://www.nodeflair.com/api/v2/jobs?query=${searchStr}&page=${pg}`)
-    const data = await response.json()
-    let result = []
-    data.job_listings.forEach((job,idx) => {
-        result[idx] = {
-            company: job.company.companyname,
-            title: job.title,
-            position: job.position,
-            experience: job.seniority[0] ? job.seniority[0] : '',
-            salary_min: job.salary_min ? job.salary_min : '',
-            salary_max: job.salary_max,
-            currency: job.currency,
-            skills: job.tech_stacks,
-            link: 'https://www.nodeflair.com'+job.job_path
-        }
-    })
-    res.json(result)
-    return
-},
+    const response = await fetch(
+      `https://www.nodeflair.com/api/v2/jobs?query=${searchStr}&page=${pg}`
+    );
+    const data = await response.json();
+    let result = [];
+    data.job_listings.forEach((job, idx) => {
+      result[idx] = {
+        company: job.company.companyname,
+        title: job.title,
+        position: job.position,
+        experience: job.seniority[0] ? job.seniority[0] : "",
+        salary_min: job.salary_min ? job.salary_min : "",
+        salary_max: job.salary_max,
+        currency: job.currency,
+        skills: job.tech_stacks,
+        link: "https://www.nodeflair.com" + job.job_path,
+      };
+    });
+    res.json(result);
+    return;
+  },
 
   postJob: async (req, res) => {
     // perform validations
-    let token = res.locals.userAuth
-    console.log('token: ', token)
-    let Id = token.data.id
-    console.log('Id:', Id)
-    let userId = mongoose.Types.ObjectId(Id)
-    console.log('userId: ', userId)
-    
+    let token = res.locals.userAuth;
+    console.log("token: ", token);
+    let Id = token.data.id;
+    console.log("Id:", Id);
+    let userId = mongoose.Types.ObjectId(Id);
+    console.log("userId: ", userId);
 
     const validationResults = jobVal.createJobs.validate(req.body);
     console.log("validationResults:", validationResults);
@@ -61,7 +61,7 @@ module.exports = {
     console.log("validatedResults: ", validatedResults);
 
     try {
-      console.log('userId2:', userId)
+      console.log("userId2:", userId);
       await postJobModel.create({
         user: userId,
         company: validatedResults.company,
@@ -88,15 +88,15 @@ module.exports = {
     res.json(allPostedJobs);
   },
 
-  listUserPostedJobs: async (req,res) => {
+  listUserPostedJobs: async (req, res) => {
     // list all jobs that user has posted
-    const token = res.locals.userAuth
-    const id = mongoose.Types.ObjectId(token.data.id)
+    const token = res.locals.userAuth;
+    const id = mongoose.Types.ObjectId(token.data.id);
     try {
-    const userPostedJobs = await postJobModel.find({user : id})
-    res.json(userPostedJobs)
-    } catch(err) {
-      res.json("You have not posted any jobs")
+      const userPostedJobs = await postJobModel.find({ user: id });
+      res.json(userPostedJobs);
+    } catch (err) {
+      res.json("You have not posted any jobs");
     }
   },
 
@@ -136,17 +136,17 @@ module.exports = {
     // find and delete job from database
     const id = req.params.id;
     await postJobModel.findByIdAndDelete(id);
-    console.log('delete successful')
-    res.json('delete successfull')
+    console.log("delete successful");
+    res.json("delete successfull");
   },
 
   listSavedJobs: async (req, res) => {
     // list all jobs in JSON format
-    const token = res.locals.userAuth
-    let userId = mongoose.Types.ObjectId(token.data.id)
-    const filter = { user: userId }
+    const token = res.locals.userAuth;
+    let userId = mongoose.Types.ObjectId(token.data.id);
+    const filter = { user: userId };
 
-    const savedJobData = await savedJobModel.find(filter)
+    const savedJobData = await savedJobModel.find(filter);
     res.json(savedJobData);
   },
 
@@ -156,38 +156,38 @@ module.exports = {
 
   saveJob: async (req, res) => {
     // save job data listed via listJobs() into database
-    const saveId = req.body.id
-    const token = res.locals.userAuth
-    let userId = mongoose.Types.ObjectId(token.data.id)
+    const saveId = req.body.id;
+    const token = res.locals.userAuth;
+    let userId = mongoose.Types.ObjectId(token.data.id);
 
-    const filter = { user: userId }
-    const update = { $push: { jobId : saveId } }
+    const filter = { user: userId };
+    const update = { $push: { jobId: saveId } };
 
     //validation
-    const validationAcc = await savedJobModel.find(filter)
+    const validationAcc = await savedJobModel.find(filter);
 
     if (validationAcc === undefined) {
       await savedJobModel.create({
         user: userId,
-        jobId: null
-      })
+        jobId: null,
+      });
     } else {
       try {
         if (validationAcc[0].jobId.includes(saveId)) {
-          return res.json("Job exists in saved database")
+          return res.json("Job exists in saved database");
         }
       } catch (err) {
-        console.log("User saved data does not exist: ",err)
+        console.log("User saved data does not exist: ", err);
       }
     }
 
     //adding job ID to savedJobs DB
     await savedJobModel.findOneAndUpdate(filter, update, {
       new: true,
-      upsert: true
-    })
+      upsert: true,
+    });
 
-    return res.json("Job Saved!")
+    return res.json("Job Saved!");
   },
 
   removeSavedJob: async (req, res) => {
@@ -196,4 +196,3 @@ module.exports = {
     await savedJobModel.findByIdAndDelete(id);
   },
 };
-
